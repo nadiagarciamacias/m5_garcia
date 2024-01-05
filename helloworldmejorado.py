26926347d68ce0d1df8930bbe7c526f86cb1acd8
nadiagarciamacias <nadiagarcia@iesmontsia.org>
2024-01-05 09:38:50 +0100

    Nova proposta

---
+++ helloworldmejorado.py
@@ -0,0 +1,5 @@
+# Pide al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Saluda al usuario con un mensaje personalizado
+print(f"¡Hola, {nombre}! Bienvenido al mundo de Python.")
